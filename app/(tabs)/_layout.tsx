5f73a9efa39490faf8c414baff6447e7e3113aa1
Ulises Pellegrini <ulises_pellegrini@outlook.com>
2025-02-01 09:43:15 -0300

    Agregar barra de navegacion al perfil de usuario

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -28,8 +28,8 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Registro',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: 'Usuario',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.fill" color={color} />,
         }}
       />
       <Tabs.Screen
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,36 +1,8 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import React from 'react';
+import User from '@/screens/UserDashboard';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import FormScreen from '@/screens/FormScreen';
-
-export default function HomeScreen() {
-  //return (
-   // <FormScreen />
- // );
+export default function AdmScreen() {
+  return (
+    <User />
+  );
 }
-
-
-
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
-
