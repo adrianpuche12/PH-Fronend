f438bccf82087ab60c74f75664d7dad5b932ddda
PiaLeonor12 <jorgeadrian_pucheta@hotmail.com>
2025-04-05 14:52:50 -0300

    cambios para intentar solucionar el error de not found cada vez que el cliente refresca la pagina intento 2

--- navigation/StackNavigator.tsx
+++ navigation/StackNavigator.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import FormScreen from '../screens/FormScreen';
-import TransactionsScreen from '../screens/TransactionsScreen';  
+import TransactionsScreen from '../screens/TransactionsScreen';
 import DynamicFormScreen from '@/screens/DynamicFormScreen';
 import AdminScreen from '../screens/AdminScreen';
 import AdminDashboard from '@/screens/AdminDashboard';
@@ -11,14 +11,12 @@ const Stack = createStackNavigator();
 
 export default function StackNavigator() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Form">
-        <Stack.Screen name="Form" component={FormScreen} />
-        <Stack.Screen name="Transactions" component={TransactionsScreen} />  
-        <Stack.Screen name="DynamicFormScreen" component={DynamicFormScreen} />
-        <Stack.Screen name="AdminScreen" component={AdminScreen} />
-        <Stack.Screen name="AdminDashboard" component={AdminDashboard} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator initialRouteName="Form">
+      <Stack.Screen name="Form" component={FormScreen} />
+      <Stack.Screen name="Transactions" component={TransactionsScreen} />
+      <Stack.Screen name="DynamicFormScreen" component={DynamicFormScreen} />
+      <Stack.Screen name="AdminScreen" component={AdminScreen} />
+      <Stack.Screen name="AdminDashboard" component={AdminDashboard} />
+    </Stack.Navigator>
   );
 }
