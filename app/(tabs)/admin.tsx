13e78b1e121c1676232a6740703a231655779504
leonel2789 <leofer-acosta.755@hotmail.com>
2025-07-08 18:40:29 -0300

    Update admin.tsx

--- app/(tabs)/admin.tsx
+++ app/(tabs)/admin.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Admin from '@/screens/AdminScreen';
+import Admin from '../../screens/AdminScreen';
 
 export default function AdminScreen() {
   return <Admin />;
